856478d3726d28a8b533ca6a1c0c6425995de3c5
danielmxPeck <danielxuan98@gmail.com>
2025-08-11 00:37:34 +0800

    exercise 4 & exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -7,12 +7,18 @@ public class Exercise4 {
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono()
-
-        // Print the value from intNumberMono when it emits
+        // Print the value from intNumberMono when it emits 
         // TODO: Write code here
+        ReactiveSources.intNumberMono()
+                .subscribe(num -> System.out.println("Value from Mono: " + num),
+                        error -> System.out.println(error.getMessage()),
+                        () -> System.out.println("Mono completed"));
+
 
         // Get the value from the Mono into an integer variable
         // TODO: Write code here
+        Integer intValue = ReactiveSources.intNumberMono().block();
+        System.out.println("Value from Mono: " + intValue);
 
         System.out.println("Press a key to end");
         System.in.read();
--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -10,12 +13,32 @@ public class Exercise5 {
 
         // Subscribe to a flux using the error and completion hooks
         // TODO: Write code here
+//        ReactiveSources.intNumberMono().subscribe(
+//                num -> System.out.println(num),
+//                error -> System.out.println("Error: " + error.getMessage()),
+//                () -> System.out.println("Mono completed")
+//        );
+
 
         // Subscribe to a flux using an implementation of BaseSubscriber
         // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribed happened");
+        request(1); // Request one item
+    }
+
+    public void hookOnNext(T item) {
+        System.out.println(item.toString() + " received");
+        request(1);
+    }
 }
